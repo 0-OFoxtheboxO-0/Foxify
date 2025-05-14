8eb7b9fffea138a86dedaa045999e150744dc146
0-OFoxtheboxO-0 <foxthebox2009@gmail.com>
2025-05-14 00:45:14 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot.run(TOKEN)
